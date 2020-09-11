5e17eaef54dc739fe4507d67b4edd7b2315ae52d
MichaelTc <mitrocollazos@unicauca.edu.co>
2020-09-11 18:49:07 -0500

    Constructor

---
+++ src/constructor/Nombre.java
@@ -0,0 +1,16 @@
+
+package constructor;
+
+/**
+ * @author MichaelTc
+ */
+public class Nombre {
+
+            public String nombre,apellido;
+            
+ public Nombre(String nombre, String apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+ }   
+ 
+}
---
+++ src/constructor/Princiapal.java
@@ -0,0 +1,15 @@
+
+
+package constructor;
+
+
+public class Princiapal {
+
+  
+    public static void main(String[] args) {
+
+        Nombre michael =new Nombre("Michael","Trochez");
+        
+        System.out.println("Mi nombre es: "+michael.nombre+" y mi apellido es: "+michael.apellido);
+    }
+}
